a2df7a164b02d00d7d00af17b2a6f3f213797838
siwar.ahmadi@esprit.tn <siwar.ahmadi@esprit.tn>
2023-02-11 19:01:00 +0100

    connexion JDBC

---
+++ src/pidev/Pidev.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidev;
+
+import utils.connexionDB;
+
+/**
+ *
+ * @author Siwar Ahmadi
+ */
+public class Pidev {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        connexionDB db1 = connexionDB.getInstance(); 
+        connexionDB db2 = connexionDB.getInstance(); 
+        
+        
+         
+    }
+    
+}
---
+++ src/utils/connexionDB.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Siwar Ahmadi
+ */
+public class connexionDB { 
+    final String url ="jdbc:mysql://localhost:3306/soundon";
+    final String login ="root";
+    final String pwd="";
+    private static connexionDB instance;
+    Connection connexion;
+    
+    
+    private connexionDB(){
+        
+        try {
+            connexion =  DriverManager.getConnection(url, login, pwd);
+            System.out.println("Connexion établie!");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    
+    public static connexionDB getInstance(){
+    if (instance == null)
+        instance = new connexionDB();
+    return instance;
+    }
+
+    public Connection getConnexion() {
+        return connexion;
+    }
+    
+}
